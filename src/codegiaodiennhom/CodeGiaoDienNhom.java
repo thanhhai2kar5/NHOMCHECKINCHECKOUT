cd28c94ff1608327c44c4661300407020f984e8a
Nguyễn Việt Anh <154953856+an67hjiiiii@users.noreply.github.com>
2025-02-20 17:17:23 +0700

    Update file
    Cập nhật chức năng Thêm

--- src/codegiaodiennhom/CodeGiaoDienNhom.java
+++ src/codegiaodiennhom/CodeGiaoDienNhom.java
@@ -18,6 +18,7 @@ public class CodeGiaoDienNhom extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
         // Panel nhập dữ liệu
+        // Cập nhật chức năng Thêm
         JPanel inputPanel = new JPanel(new GridLayout(2, 1, 10, 10));
         JLabel titleLabel = new JLabel("Nhập thông tin ", SwingConstants.CENTER);
         textField = new JTextField();
@@ -30,7 +31,7 @@ public class CodeGiaoDienNhom extends JFrame {
         add(new JScrollPane(displayList), BorderLayout.CENTER);
         // Panel chứa các nút chức năng
         JPanel buttonPanel = new JPanel(new GridLayout(1, 3, 10, 10));
-        JButton addButton = new JButton("Thêm "); // Việt Anh
+        JButton addButton = new JButton("Thêm"); // Việt Anh
         buttonPanel.add(addButton);
         add(buttonPanel, BorderLayout.SOUTH);
         // Chức năng Thêm - Việt Anh
